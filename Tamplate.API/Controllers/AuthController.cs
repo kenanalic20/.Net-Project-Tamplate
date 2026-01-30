a0434e69888deb4a85370ee089fbcb2c6839510e
kenanalic20 <kenan.alic@edu.fit.ba>
2026-01-30 01:38:55 +0100

    Added missing controllers

---
+++ Tamplate.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tamplate.Application.DTOs;
+using Tamplate.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Tamplate.Application.Common;
+using Tamplate.Messages;
+
+namespace Tamplate.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(
+            IAuthService authService
+        )
+        {
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto register)
+        {
+            var token = await _authService.RegisterAsync(register);
+            return Ok(new { token });
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto login)
+        {
+            var token = await _authService.LoginAsync(login);
+            return Ok(new { token });
+        }
+
+        [Authorize]
+        [HttpGet("user")]
+        public async Task<IActionResult> GetUser()
+        {
+            var userDto = await _authService.GetUserAsync(User);
+            return Ok(new { user = userDto });
+        }
+        
+
+    }
+}
---
+++ Tamplate.API/Controllers/BaseController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tamplate.Application.Interfaces.Services;
+
+namespace Tamplate.API.Controllers
+{
+    [Route("[controller]")]
+    public class BaseController<T, TDto, TCreateDto, TUpdateDto, TQueryFilter, TKey> : ControllerBase
+        where T : class
+        where TDto : class
+        where TCreateDto : class
+        where TUpdateDto : class
+        where TQueryFilter : class
+    {
+        private readonly IService<T, TDto, TCreateDto, TUpdateDto, TQueryFilter, TKey> _service;
+
+        public BaseController(IService<T, TDto, TCreateDto, TUpdateDto, TQueryFilter, TKey> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public virtual async Task<IActionResult> GetById(TKey id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> GetAll([FromQuery] TQueryFilter? queryFilter = null)
+        {
+            var result = await _service.GetAllAsync(queryFilter);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Create([FromBody] TCreateDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _service.CreateAsync(dto);
+           
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<IActionResult> Update(TKey id, [FromBody] TUpdateDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _service.UpdateAsync(id, dto);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete(TKey id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
