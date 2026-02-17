7317a4ca86044a0e2331968d30d83562e9a91791
kenanalic20 <kenan.alic@edu.fit.ba>
2026-02-17 02:57:12 +0100

    #6

---
+++ Tamplate.API/wwwroot/file-manager/js/app.js
@@ -0,0 +1,17 @@
+class App{
+   
+    constructor() {
+        this.init();
+    }
+
+    init(){
+        console.log("Works");
+        this.auth();
+    }
+
+    auth(){
+        
+    }
+   
+}
+var app = new App();
---
+++ Tamplate.API/wwwroot/file-manager/js/auth.js
@@ -0,0 +1,6 @@
+class Auth{
+    constructor() {
+        this.tokenKey = 'jwtToken';
+        this.userKey = 'currentUser';
+    }
+}
